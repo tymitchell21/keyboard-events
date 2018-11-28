83168a8f1a15d63b1856b315c1742368e68773b5
Tyler <tymitchell2100@gmail.com>
2018-11-28 11:58:16 -0500

    adds pacman with finished product

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 'use strict';
 
+let boxTop = 200;
+let boxLeft = 200;
+
 document.addEventListener('keydown', (event) => {
   const keyName = event.key;
   console.log('keydown event\n\n' + 'key: ' + keyName);
+
+  if (keyName == 'ArrowDown') {
+    boxTop+=10;
+  } else if (keyName == 'ArrowUp') {
+    boxTop-=10;
+  } else if (keyName == 'ArrowLeft') {
+    boxLeft-=10;
+  } else if (keyName == 'ArrowRight') {
+    boxLeft+=10;
+  }
+
+  document.getElementById("box").style.top = boxTop + "px";
+  document.getElementById("box").style.left = boxLeft + "px";
 });
